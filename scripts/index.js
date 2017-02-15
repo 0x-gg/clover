96168f2c2616562b2435a630f2560e1008b84a67
Gaurav Gada <gauravgada@gmail.com>
2017-02-15 14:39:02 -0800

    Latest Changes
    Added localStorate
    Play/Pause with p
    Green background initialization
    Fixed bounce logic
    Each box moves independently

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,44 @@ var randomMove = document.querySelector('#randomMove');
 
 var bodyObj = document.querySelector('body');
 
+var idArr = [];
+var idMap = {};
+
+function getParameterByName(name, url) {
+    if (!url) {
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+
+var shapeColor = getParameterByName('color');
+if (shapeColor == null) {
+    shapeColor = localStorage.getItem('color');
+} else {
+    localStorage.setItem('color', shapeColor);
+}
+var boxes = getParameterByName('boxes');
+if (boxes == null) {
+    boxes = localStorage.getItem('boxes');
+} else {
+    localStorage.setItem('boxes', boxes);
+}
+
+function getRGBAColor(colorStr) {
+    if (colorStr === 'none') return 'none';
+    var a = document.createElement('div');
+    a.style.color = colorStr;
+    var colors = window.getComputedStyle(document.body.appendChild(a)).color;
+    document.body.removeChild(a);
+    return colors.replace(/\)/, ', 0.1)').replace('rgb', 'rgba');
+}
 
 function createBox() {
     let newBox = document.createElement('div');
@@ -22,11 +60,26 @@ function createBox() {
     newBox.style.width = boxAttr.shift() + 'px';
     newBox.style.height = newBox.style.width;
     newBox.style.borderRadius = Math.floor(Math.random() * 40) + '%';
+    let newID = idArr.length;
+    idArr.push(newID + 1);
+    idMap["box-" + (newID + 1)] = [1, 1];
+    // console.log(idMap);
+    newBox.id = "box-" + (newID + 1);
     // newBox.style.display = 'block';
     bodyObj.appendChild(newBox);
     newBox.appendChild(textContainer);
     textContainer.innerHTML = textBox.value;
+    if (shapeColor !== null) {
+        newBox.style.backgroundColor = getRGBAColor(shapeColor);
+    }
     newBox.addEventListener('click', alertUser);
+}
+
+console.log(boxes);
+if (boxes !== null) {
+    for (i = 0; i < boxes; i++) {
+        createBox();
+    }
 }
 
 function updateText() {
@@ -34,21 +87,6 @@ function updateText() {
     let boxes = document.querySelectorAll('.boxy-text');
     boxes.forEach(b => b.innerHTML = this.value);
 }
-
-function getParameterByName(name, url) {
-    if (!url) {
-        url = window.location.href;
-    }
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-var shapeColor = getParameterByName('color');
-console.log(shapeColor);
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -66,10 +104,10 @@ function genRandomAttr() {
     w > h ? pixelWidth = Math.floor(Math.random() * h) : pixelWidth = Math.floor(Math.random() * w);
     let posX = Math.floor(w * Math.random());
     let posY = Math.floor(h * Math.random());
-    console.log("before: ", w, h, posX, posY, pixelWidth);
+    // console.log("before: ", w, h, posX, posY, pixelWidth);
     if (posX + pixelWidth >= w) posX -= pixelWidth - (w - posX);
     if (posY + pixelWidth >= h) posY -= pixelWidth - (h - posY);
-    console.log("after: ", w, h, posX, posY, pixelWidth);
+    // console.log("after: ", w, h, posX, posY, pixelWidth);
 
     let letters = '0123456789ABCDEF';
     let color = '#';
@@ -79,33 +117,6 @@ function genRandomAttr() {
     return [posX, posY, color, pixelWidth];
 }
 
-function toggleShape() {
-    if (isElementHidden(shape)) {
-        let innerShape = document.querySelector('.shape');
-        let shape = document.querySelector('.shape-container');
-        innerShape.style.borderColor = getRandomColor();
-        let randomRadius = Math.random() * 50;
-        innerShape.style.borderRadius = randomRadius + '%';
-        // let boxCoords = genRandomCoords();
-        console.log(boxCoords);
-        shape.style.position = 'absolute';
-        shape.style.left = boxCoords.shift() + 'px';
-        shape.style.top = boxCoords.shift() + 'px';
-        shape.style.display = 'block';
-
-        if (shapeColor !== 'none') {
-            innerShape.style.backgroundColor = shapeColor;
-            innerShape.style.opacity = "0.4";
-        }
-    } else {
-        shape.style.display = 'none';
-    };
-
-    function isElementHidden(element) {
-        return window.getComputedStyle(element, null).getPropertyValue('display') === 'none';
-    }
-}
-
 function clearAll() {
     let boxes = document.querySelectorAll('.boxy-thing');
     boxes.forEach(b => b.style.display = 'none');
@@ -113,10 +124,8 @@ function clearAll() {
     // textBox.value = '';
 }
 
-
-
 function alertUser(e) {
-    console.log(e);
+    // console.log(e);
     let color = e.target.style["border-top-color"];
     let text = e.target.firstChild.innerHTML;
     let alertMessage = 'Color: ' + color + '\n' + 'Text: ' + text;
@@ -150,26 +159,39 @@ var tc = 1;
 var lc = 1;
 
 function moveBox(e) {
-    // console.log(b);
+
     var top = parseInt(e.style.top);
     var left = parseInt(e.style.left);
     var width = parseInt(e.style.width);
-
     let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
     // check boundary
 
-    if (top <= 0) tc = 1;
-    if (left <= 0) lc = 1;
+    if (top <= 0) {
+        // tc = 1;
+        idMap[e.id][1] = 1; //Y
+    }
+    if (left <= 0) {
+        // lc = 1;
+        idMap[e.id][0] = 1; //X
+    }
 
-    if (left + width >= w) lc = -1;
-    if (top + width >= h) tc = -1;
-    console.log(tc, lc, top + tc + 'px');
-    e.style.top = top + tc + 'px';
-    e.style.left = left + lc + 'px';
+    if (left + width >= w) {
+        // lc = -1;
+        idMap[e.id][0] = -1; //X
+    }
+    if (top + width >= h) {
+        // tc = -1;
+        idMap[e.id][1] = -1; //Y
+    }
 
-    return;
+    e.style.top = top + idMap[e.id][1] + 'px';
+    e.style.left = left + idMap[e.id][0] + 'px';
+}
+
+function toggleP(e) {
+    if (document.activeElement.id !== 'txtJob' && e.keyCode == 80) startRandomMove();
 }
 
 myBtn.addEventListener('click', createBox);
@@ -177,4 +199,4 @@ randomMove.addEventListener('click', startRandomMove);
 clearBtn.addEventListener('click', clearAll);
 textBox.addEventListener('change', updateText);
 textBox.addEventListener('keyup', updateText);
-// shape.addEventListener('click', alertUser);
+window.addEventListener('keydown', toggleP);
